c3188e3b8ff15bc5a07af55c581dbca1a4e0b94a
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-03-03 14:10:46 +0100

    Prechod na styled components a pridanie UI componentov plus cesty  - routes react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Rides from "./pages/Rides";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Bookings from "./pages/Bookings";
+import GlobalStyles from "./styles/GlobalStyles";
+
 export default function App() {
   return (
-    <h1 className="bg-gray-50 text-3xl font-bold underline">
-      Carriage Ride Dashboard
-    </h1>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="rides" element={<Rides />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
