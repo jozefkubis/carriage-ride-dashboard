329a9f07a965e18ee3127d75468e96785300b010
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-03-11 14:20:55 +0100

    Pridane polia do settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,9 +4,10 @@ import Textarea from "../../components/Textarea";
 import { useSettings } from "./useSettings";
 import { useUpdateSetting } from "./useUpdateSetting";
 import Spinner from "../../components/Spinner";
+import Input from "../../components/Input";
 
 function UpdateSettingsForm() {
-  const { isLoading, settings: { address } = {} } = useSettings();
+  const { isLoading, settings: { address, email, phone } = {} } = useSettings();
   const { isUpdating, updateSetting } = useUpdateSetting();
 
   if (isLoading) return <Spinner />;
@@ -20,6 +21,26 @@ function UpdateSettingsForm() {
 
   return (
     <Form>
+      <FormRow label="Email">
+        <Input
+          type="email"
+          id="email"
+          defaultValue={email}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "email")}
+        />
+      </FormRow>
+
+      <FormRow label="Telefón">
+        <Input
+          type="tel"
+          id="phone"
+          defaultValue={`+421 ${phone}`}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "phone")}
+        />
+      </FormRow>
+
       <FormRow label="Adresa sídla (ulica, číslo domu, mesto, smerovacie číslo, krajina)">
         <Textarea
           id="address"
